0374d0d3f766b9e01e300bedb1a8ff2ae8247911
Gandalf <h1c2p3m4@gmail.com>
2025-01-30 12:06:45 -0500

    Inicio de los archivos en la carpeta

---
+++ app.js
@@ -0,0 +1,3 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//En una lista vacía se van a almacenar los nombres.
+let amigos = []
