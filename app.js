6bb12b1c6cb2b92ee7eac14861fc68717cf5b3c4
Gandalf <h1c2p3m4@gmail.com>
2025-01-30 16:02:11 -0500

    Agregamos una función

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-//En una lista vacía se van a almacenar los nombres.
-let amigos = []
+//Lista donde se va a almacenar los nombres.
+let amigos = [];
+ //Función para que el usuario pueda ingresar nombres y añadirlo a la lista.
+function agregarAmigo(){
+    let usuarioAmigos = document.getElementById('amigo').value;
+    console.log(usuarioAmigos);
+    //El return detiene la ejecución de la función en el punto en el que se encuentra. Cualquier código
+    //  que venga después del return nunca se ejecutará.
+    return
+ }
